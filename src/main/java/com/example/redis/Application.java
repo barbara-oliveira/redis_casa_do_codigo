6043ccc05561dea60100b3b7f9af4d5e04e5ffde
barbara <barbara31fab@gmail.com>
2020-09-06 19:27:01 -0300

    first commands with jedis

---
+++ src/main/java/com/example/redis/Application.java
@@ -0,0 +1,22 @@
+package com.example.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class Application {
+    public static void main(String [] args) {
+
+        Jedis jedis = new Jedis("localhost");
+        String resultadoEcho = jedis.echo("ola redis!");
+        System.out.println(resultadoEcho);
+
+        String resultadoSet = jedis.set("ultimo_usuario_logado", "Mulher-Maravilha");
+        System.out.println(resultadoSet);
+
+        String resultadoGet = jedis.get("ultimo_usuario_logado");
+        System.out.println(resultadoGet);
+
+        String resultadoDel = String.valueOf(jedis.del("ultimo_usuario_logado"));
+        System.out.println(resultadoDel);
+
+    }
+}
